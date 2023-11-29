17d600d74b0d418d85a8be581b1dd7b6ea4858a9
smile1130 <superskiller1130@gmail.com>
2023-11-29 22:32:11 +0900

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import "./App.css";
 import CampaignContainer from "./container/CampaignContainer";
 import { CampaignContextProvider } from "./context/CampaignContextProvider";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // container
 
 function App() {
     return (
         <div className="App">
             <CampaignContextProvider>
-                <CampaignContainer />
+                <Router>
+                    <Routes>
+                        <Route
+                            path="/campaigns"
+                            element={<CampaignContainer />}
+                        />
+                        {/* Add more Routes as needed */}
+                    </Routes>
+                </Router>
             </CampaignContextProvider>
         </div>
     );
